478c25320a228565a622f1d13219ede5a62122a3
Aderito Baptista <a.baptista@elsevier.com>
2019-09-05 15:30:17 +0100

    clean up

--- app_java/src/main/java/com/example/appjava/EnvApiApplication.java
+++ app_java/src/main/java/com/example/appjava/EnvApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleapiApplication {
+public class EnvApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SimpleapiApplication.class, args);
+		SpringApplication.run(EnvApiApplication.class, args);
 	}
 
 }
